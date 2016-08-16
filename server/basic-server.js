1c058f309bb0497ce3b51e059a4f5e66d4d5e9bd
cihannisanci <cihannisan@gmail.com>
2016-08-15 20:32:51 -0700

    Passed server specs

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,7 +1,7 @@
 /* Import node's http module: */
 var http = require('http');
-
-
+var handleRequest = require('./request-handler.js');
+var url = require('url');
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
 // normally already claimed by another server and/or not accessible
@@ -15,15 +15,16 @@ var port = 3000;
 var ip = '127.0.0.1';
 
 
-
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+url.pathname = '/classes/messages/';
+var server = http.createServer(handleRequest.requestHandler);
 console.log('Listening on http://' + ip + ':' + port);
+console.log('url: ', url);
 server.listen(port, ip);
 
 // To start this server, run:
--- server/request-handler.js
+++ server/request-handler.js
@@ -12,6 +12,7 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
+var data = [];
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
@@ -28,22 +29,46 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
+  // console.log('response: ', response);
   // The outgoing status.
+  var defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // Seconds.
+  };
   var statusCode = 200;
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
+  var method = request.method;
+
+  if (method === 'POST' && request.url.split('?')[0] === '/classes/messages/') {
+    request.on('data', function(chunk) {
+      data.push(JSON.parse(chunk));      
+    });
+    statusCode = 201;
+  } else if (method === 'GET' && request.url.split('?')[0] === '/classes/messages/') {
+   
+    headers['Content-Type'] = 'application/json';
+    response.writeHead(statusCode, headers);
+    var responseBody = {
+      method: method,
+      results: data
+    };
+    response.end(JSON.stringify(responseBody));
+  } else {
+    statusCode = 404;
+  }
+  
+
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,8 +77,9 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
 };
+
+exports.requestHandler = requestHandler;
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -64,10 +90,4 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
